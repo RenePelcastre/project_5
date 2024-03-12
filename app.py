e30116e36157106e028037cd2757acbadd26b3b5
RenePelcastre <rene.pelcastre@gmail.com>
2024-03-11 23:10:38 -0600

    app actualizada

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
-import streamlit as st
 import pandas as pd
 import plotly.express as px
+import streamlit as st
 
-# car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-# fig = px.histogram(car_data, x="odometer") # crear un histograma
-# fig.show() # crear gráfico de dispersión
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
-        
-if hist_button: # al hacer clic en el botón
-            # escribir un mensaje
-            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
-            # crear un histograma
-            fig = px.histogram(car_data, x="odometer")
-        
-            # mostrar un gráfico Plotly interactivo
-            st.plotly_chart(fig, use_container_width=True)
+st.header('Car Sale')
+
+hist_button = st.button('Construir histograma')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+    fig.show()
+
+# La siguiente sección se ajustó para tener una estructura coherente
+build_scatter = st.checkbox('Construir un diagráma de dispersión para observar relación: precio, odómetro y transmisión')
+
+if build_scatter:  # si la casilla de verificación está seleccionada
+    st.write('Construir un diagráma de dispersión para observar relación: precio, odómetro y transmisión')
+    # crear un histograma
+    fig = px.scatter(data_frame=car_data,
+                     x="odometer",
+                     y="price",
+                     color="transmission",
+                     width=1000,
+                     height=400,
+                     title="Relation price / odometer / transmission")
+    fig.show()
